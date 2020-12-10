9b9be460a679a77bf6909f72f54dff3cc3c8ddd1
olegkazanok <kazanokoleg468@gmail.com>
2020-12-10 08:33:40 +0300

    the created class MainFrame

--- src/com/company/MainFrame.java
+++ src/com/company/MainFrame.java
@@ -162,7 +162,6 @@ public class MainFrame extends JFrame {
 		public static void main(String[] args) {
 			// Создать и показать экземпляр главного окна приложения
 			MainFrame frame = new MainFrame();
-
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
 		}
